2e0fcba71e8d030f3d9a9939db4226c84f2dea51
GalinaUm <GalinaUmerzakhova@yandex.ru>
2026-04-08 16:32:48 +0300

    feat: docs urls

--- habits/tasks.py
+++ habits/tasks.py
@@ -15,4 +15,4 @@ def check_habits_and_notify():
     for habit in habits_to_remind:
         message = f"Напоминание: {habit.habit} в {habit.place}!"
         if habit.habit_creator.telegram_id:  # Если у юзера заполнен ID
-            send_telegram_message(habit.habit_creator.id, message)
+            send_telegram_message(habit.habit_creator.telegram_id, message)
